3052945dfc1cea6f43eecd15057e955df8a590a4
Zach Phillips <zach@thekitchen.co>
2012-01-23 02:24:35 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MzScratch::Application.config.secret_token = '5129c51dc422bc59086d36ef613d5761fa247a741f47c37502b5c29a66ea1ef4f3103e10c1433291f6e4396a21d669e94ee3656dcbef9ccc4ed8d07314803cfc'
