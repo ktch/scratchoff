dd0bbe3c28165a3d856335b1c88a4adb65f4220f
Zach Phillips <zach@thekitchen.co>
2012-02-27 02:21:30 -0500

    Refactored layouts and established main routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 MzScratch::Application.routes.draw do
 
-  devise_for :admins, :path => 'admin', :path_names => { :sign_in => 'login', :sign_up => 'new', :sign_out => 'logout', :password => 'secret', :confirmation => 'verification' }
+  devise_for :admins, :path => 'admin', :path_names => { :sign_in => 'login', :sign_up => 'new', :sign_out => 'logout', :password => 'secret', :confirmation => 'verification' }, :controllers => { :sessions => "admins/sessions" }
 
-  root :to => 'ticket#generate'
+  match '/dashboard', :to => 'admins#dashboard'
+    
+  root :to => 'tickets#generate'
   resources :prizes
 
 end
