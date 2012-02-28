1c361f8fd9adec9b5698f0dfe729e08b5c9d3dbd
Zach Phillips <zach@thekitchen.co>
2012-02-28 02:22:52 -0500

    Added a Sessions controller and a sign in view

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 MzScratch::Application.routes.draw do
+  
+  resources :admins
+  resources :prizes
+  resources :sessions, :only => [:new, :create, :destroy]
+  
+  root :to => 'tickets#generate'
 
   match '/dashboard', :to => 'admins#dashboard'
-    
-  root :to => 'tickets#generate'
-  resources :prizes
-  resources :admins
+  match '/signup',    :to => 'admins#new'
+  match '/signin',    :to => 'sessions#new'
+  match '/signout',   :to => 'sessions#destroy'
 
 end
